2b8378b39b6ea8840d7af50e85447b59c401a3a3
muraki <muraki@triax.jp>
2012-07-07 16:51:38 +0900

    修正

--- test/__init__.py
+++ test/__init__.py
@@ -7,15 +7,18 @@ class TestBoard:
     def __init__(self):
         self.board = Board()
 
-    def testInstantiation(self):
+    def test_instantiation(self):
         assert self.board
 
-    def testGet(self):
+    def test_get(self):
         assert self.board.get(3, 3)
 
-    def testInitialAssignment(self):
+    def test_initial_assignment(self):
         assert self.board.get(3, 3) == BLACK
         
-# 関数でテストを書く
-def test_compare():
-    assert 1==1
+
+def test_find_turnable_points():
+    board = Board()
+    print find_turnable_points(board, BLACK)
+
+test_find_turnable_points()
