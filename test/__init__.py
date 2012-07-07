bd61d49580b1bd2a12dca87277f49de9aaa45056
muraki <muraki@triax.jp>
2012-07-07 15:37:35 +0900

    noseでテストのテストを記述

---
+++ test/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+test for test
+"""
+
+# クラスでテストを書く
+class TestFoo:
+    def testCompare(self):
+        assert 1==1
+
+# 関数でテストを書く
+def test_compare():
+    assert 1==1
