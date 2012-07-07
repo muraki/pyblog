37ffec2dc90f99a09ec33064ea554a1bf1fc3c60
muraki <muraki@triax.jp>
2012-07-07 16:16:16 +0900

    add unit test for othello board.

--- test/__init__.py
+++ test/__init__.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""
-test for test
-"""
+from othello import *
 
-# クラスでテストを書く
-class TestFoo:
-    def testCompare(self):
-        assert 1==1
+# Test for Board class
+class TestBoard:
+    def __init__(self):
+        self.board = Board()
 
+    def testInstantiation(self):
+        assert self.board
+
+    def testGet(self):
+        assert self.board.get(3, 3)
+
+    def testInitialAssignment(self):
+        assert self.board.get(3, 3) == BLACK
+        
 # 関数でテストを書く
 def test_compare():
     assert 1==1
